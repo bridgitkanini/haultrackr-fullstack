94c3d3cccc5a58656b895297f6490686c250c8be
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-07-07 06:20:16 +0300

    Add authentication classes to schema view in URLs configuration
    - Included SessionAuthentication and BasicAuthentication in the schema view for enhanced security.
    - Updated the authentication_classes attribute to an empty list to allow for custom authentication handling.

--- apps/backend/haultrackrbackend/urls.py
+++ apps/backend/haultrackrbackend/urls.py
@@ -12,6 +12,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from users.views import CustomTokenObtainPairView
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -21,6 +22,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
+    authentication_classes=[],
 )
 
 urlpatterns = [
